c37b8fffccb944c81e0afdf63dfb0f9d111d81ff
Lars Henrik Laustsen <LLA@pfa.dk>
2019-03-04 13:43:13 +0100

    Adhoc parsing refactored with regular expresssion

--- AoC2015/Day06.fsx
+++ AoC2015/Day06.fsx
@@ -1,31 +1,38 @@
 ﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\Input\Day06.txt")
 
-type rectangle = 
+type rectangle =
     { lux : int; luy :int; rlx : int ; rly : int}
     member this.Contains x = fst x >= this.lux && fst x <= this.rlx && snd x >= this.luy && snd x <= this.rly
 
-type command = | On | Off | Toggle 
+type command = | On | Off | Toggle
 
 type Instruction = {command : command; rectangle : rectangle}
 
-let parse (str : string)  =
-    let s = str.Replace("turn on","On").Replace("turn off","Off").Split(' ')
-    let command = 
-        match s.[0] with
-        | "On" -> On
-        | "Off" -> Off
-        | _ -> Toggle
-    let lu = s.[1].Split(',')
-    let rl = s.[3].Split(',')
-    let rectangle = {lux = lu.[0] |> int; luy = lu.[1] |> int; rlx = rl.[0] |> int; rly = rl.[1] |> int}
-    {command = command; rectangle = rectangle}
+let parseCommand (cmdStr : string) =
+    match cmdStr with
+    | "turn on" -> On
+    | "turn off" -> Off
+    | "toggle" -> Toggle
+    | _ -> failwith (sprintf "Unable to parse command: %s" cmdStr)
+
+let parse (str : string) =
+    let pattern = "(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)"
+    let regex = System.Text.RegularExpressions.Regex(pattern)
+    let m = regex.Match(str)
+    let intGroup (n:int) = m.Groups.[n].Value |> int
+    if m.Success then
+        let command = parseCommand (m.Groups.[1].Value)
+        let rectangle = {lux = intGroup 2; luy = intGroup 3; rlx = intGroup 4; rly = intGroup 5}
+        {command = command; rectangle = rectangle}
+    else
+        failwith (sprintf "Unable to parse '%s'" str)
 
 let instructions = input |> Array.map parse
 
 let determine1 (a,b) tændt instruction=
     match instruction.rectangle.Contains (a,b) with
     | false -> tændt
-    | true -> 
+    | true ->
         match instruction.command with
         | On -> true
         | Off -> false
@@ -34,7 +41,7 @@ let determine1 (a,b) tændt instruction=
 let determine2 (a,b) tændt instruction=
     match instruction.rectangle.Contains (a,b) with
     | false -> tændt
-    | true -> 
+    | true ->
         match instruction.command with
         | On -> tændt + 1
         | Off -> max (tændt - 1) 0
@@ -42,7 +49,7 @@ let determine2 (a,b) tændt instruction=
 
 let pairs = [0..999] |> List.collect(fun i -> [0..999] |> List.map(fun j -> (i,j)))
 
-let question1 = 
+let question1 =
     pairs |> List.map(fun x -> instructions |> Array.fold (determine1 x) false) |> List.filter id |> List.length
-let question2 = 
+let question2 =
     pairs |> List.map(fun x -> instructions |> Array.fold (determine2 x) 0) |> List.sum
