cc738edf854fcc86d0ae4d54cb635c06089a0a00
Delenda <you@example.com>
2019-12-02 06:36:01 +0100

    Advent of Code 2019

---
+++ AoC2019/Day01.fsx
@@ -0,0 +1,15 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\Input\Day01.txt")
+
+let part1 = 
+    input
+    |> Array.map int
+    |> Array.sumBy(fun mass -> mass/3 - 2)
+
+let rec totalfuel mass =
+    let fuel = mass/3 - 2
+    if fuel < 0 then 0 else fuel + totalfuel fuel
+
+let part2 = 
+    input
+    |> Array.map int
+    |> Array.sumBy totalfuel
