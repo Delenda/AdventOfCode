84af2124da1fd804e49df6e92d4feab408a33457
Delenda <lla@pfa.dk>
2020-12-01 12:03:04 +0100

    Day01

---
+++ AoC2020/Day01.fsx
@@ -0,0 +1,19 @@
+﻿let input =
+    System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + @"\Input\Day01.txt")
+    |> Array.map int
+
+let part1 =
+    seq{
+        for i in [0..input.Length-2] do
+            for j in [i+1..input.Length-1] do
+                if input.[i]+input.[j] = 2020 then yield input.[i]*input.[j]}
+    |> Seq.head
+
+
+let part2 =
+    seq{
+        for i in [0..input.Length-3] do
+            for j in [i+1..input.Length-2] do
+                for k in [j+1..input.Length-1] do
+                if input.[i]+input.[j]+input.[k] = 2020 then yield input.[i]*input.[j]*input.[k]}
+    |> Seq.head
