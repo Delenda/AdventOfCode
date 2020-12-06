85323c8add067302dd76e9d74570bcadf53585b0
Delenda <you@example.com>
2020-12-06 06:54:27 +0100

    Day06

---
+++ AoC2020/Day06.fsx
@@ -0,0 +1,17 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\Input\Day06.txt")
+
+let answers = 
+    input
+        |> Array.fold(fun s t ->
+            if t = "" then ((snd s)::fst s, "") else (fst s , (snd s) + " " + t)
+            ) ([], "")
+        |> fun (a,b) -> b::a
+
+let count setfunction = 
+    answers
+    |> Seq.map(fun s -> 
+        s.Trim().Split(' ') |> Array.map Set |> setfunction |> Set.count)
+    |> Seq.sum
+
+let part1 = count Set.unionMany
+let part2 = count Set.intersectMany
