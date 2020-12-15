69896f74cf73c6d1e26e87b2416a26b474f6bd05
Lars Henrik Laustsen <lla@pfa.dk>
2020-12-15 15:43:40 +0100

    Refactored to use more efficient datastructure

--- AoC2020/Day15.fsx
+++ AoC2020/Day15.fsx
@@ -4,40 +4,22 @@
         |> Array.toList
         |> List.map int
 
-let play  n startingnumbers =
-    startingnumbers
-    |> List.rev
-    |> Seq.unfold(fun spoken ->
-        let lastSpoken = List.head spoken
-        let newNumber =
-            spoken
-            |> List.tail
-            |> List.tryFindIndex(fun t -> t = lastSpoken)
-            |> Option.map(fun t -> t + 1)
-            |> Option.defaultValue 0
-        Some(newNumber,newNumber::spoken))
-    |> Seq.take (n - startingnumbers.Length)
-    |> Seq.last
-
-
-
-let play2  n startingnumbers =
+let play n startingnumbers =
     let cache = new System.Collections.Generic.Dictionary<int,int>()
-    startingnumbers |> List.indexed |> List.iter(fun (i,c) -> cache.Add(i,c))
-    let mutable last = -1
-    Seq.initInfinite(fun i ->
-        let idx = i + startingnumbers.Length
+    startingnumbers |> List.indexed |> List.iter(fun (i,c) -> cache.Add(c,i))
+    Seq.initInfinite(fun i -> i + startingnumbers.Length - 1)
+    |> Seq.scan (fun last lastIdx ->
         let newNumber =
             if cache.ContainsKey last then
-                idx - cache.[last]
+                lastIdx - cache.[last]
             else
                 0
         if cache.ContainsKey last then (cache.Remove last |> ignore)
-        cache.Add(last, idx - 1)
-        last <- newNumber
-        newNumber)
-    |> Seq.take (n - startingnumbers.Length)
+        cache.Add(last, lastIdx)
+        newNumber) -1
+    |> Seq.tail
+    |> Seq.take (n - startingnumbers.Length )
     |> Seq.last
 
-let part1 = play2 2020 [0;3;6]
-//let part2 = play 300000 input
+let part1 = play 2020 input
+let part2 = play 30000000 input
