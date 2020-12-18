3697c63bbfb2befec5cb9deccb43d74edb212d9b
Lars Henrik Laustsen <lla@pfa.dk>
2020-12-18 18:41:53 +0100

    Day 18 refactored

--- AoC2020/Day18.fsx
+++ AoC2020/Day18.fsx
@@ -3,95 +3,94 @@
 type symbol =
     | Digit of uint64
     | Plus
-    | Mult
-    | Left
-    | Right
+    | Asterisk
+    | Open
+    | Close
 
 let lex (line:string) =
-    line.Replace(" ", "").ToCharArray()
-    |> Array.map(fun c ->
-        match c with
-        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> c |> string |> uint64 |> Digit
-        | '+' -> Plus
-        | '*' -> Mult
-        | '(' -> Left
-        | ')' -> Right
-        | _ -> failwith (sprintf "unexpected input %s" (c.ToString())))
-    |> Array.toList
+    line
+        .ToCharArray()
+        |> Array.filter(fun c -> c <> ' ')
+        |> Array.map(fun c ->
+            match c with
+            | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> c |> string |> uint64 |> Digit
+            | '+' -> Plus
+            | '*' -> Asterisk
+            | '(' -> Open
+            | ')' -> Close
+            | _ -> failwith (sprintf "unexpected input '%s'" (c.ToString())))
+        |> Array.toList
 
 type expression =
     | Const of uint64
     | Multiplication
     | Addition
-    | SubExpression of expression list
+    | Expression of expression list
 
 type state =
     {
-        Res: expression list
-        Remain : symbol list
+        Result: expression list
+        Stream : symbol list
         Stop : bool
     }
 
 let parse (line:string) =
     let rec innerParse (symbols:symbol list) =
         Seq.unfold(fun s ->
-            if s.Remain.IsEmpty || s.Stop then None else
+            if s.Stream.IsEmpty || s.Stop then None else
             let newState =
-                match s.Remain.Head with
-                | Digit a -> {Res = Const a::s.Res; Remain = s.Remain.Tail; Stop = false}
-                | Mult -> {Res = Multiplication::s.Res; Remain = s.Remain.Tail; Stop = false}
-                | Plus -> {Res = Addition::s.Res; Remain = s.Remain.Tail; Stop = false}
-                | Left ->
-                    let t = innerParse s.Remain.Tail
-                    {Res = SubExpression t.Res::s.Res; Remain = t.Remain; Stop = false}
-                | Right ->
-                    {Res = s.Res; Remain = s.Remain.Tail; Stop = true}
+                match s.Stream.Head with
+                | Digit a   -> {Result = Const a::s.Result;         Stream = s.Stream.Tail; Stop = false}
+                | Asterisk  -> {Result = Multiplication::s.Result;  Stream = s.Stream.Tail; Stop = false}
+                | Plus      -> {Result = Addition::s.Result;        Stream = s.Stream.Tail; Stop = false}
+                | Close     -> {Result = s.Result;                  Stream = s.Stream.Tail; Stop = true}
+                | Open      ->
+                    let t = innerParse s.Stream.Tail
+                    {Result = Expression t.Result::s.Result; Stream = t.Stream; Stop = false}
             Some(newState,newState)
-            ) {Res=[]; Remain = symbols; Stop = false}
+            ) {Result=[]; Stream = symbols; Stop = false}
         |> Seq.last
-        |> fun s -> {s with Res = List.rev s.Res}
-    (line |> lex |> innerParse).Res
+        |> fun s -> {s with Result = List.rev s.Result}
+    (line |> lex |> innerParse).Result
 
 let extractValueFromConst = function
     | Const x -> x
     |_  -> failwith "Error: not a const"
 
-let rec reduce precedence_for_addition expr =
+type precedence = | Left_to_right | Addition_has_precedence
+
+let rec reduce precedence expr =
     let idx = expr |> List.tryFindIndex(fun t -> t = Addition)
-    if precedence_for_addition && idx.IsSome then
-        let reduced = expr |> List.skip (idx.Value - 1) |> List.take 3
-        let a = [List.item 0 reduced] |> reduce true |> List.head |> extractValueFromConst
-        let b = [List.item 2 reduced] |> reduce true |> List.head |> extractValueFromConst
-        (List.take (idx.Value - 1) expr)@[Const(a+b)]@(List.skip (idx.Value + 2) expr) |> reduce true
+    if precedence = Addition_has_precedence && idx.IsSome then
+        let initialSegment, tail  = expr |> List.splitAt (idx.Value - 1)
+        let addents, finalSegment = tail |> List.splitAt 3
+        let sum =
+            addents
+            |> List.filter(fun s -> s <> Addition)
+            |> List.map(fun s -> reduce precedence [s])
+            |> List.collect (List.map extractValueFromConst)
+            |> List.sum
+        initialSegment@[Const sum]@finalSegment |> reduce precedence
     else
-    match expr with
-    | Const a::[] -> [Const a]
-    | Const a::Multiplication::Const b::xs -> Const (a*b)::xs |> reduce precedence_for_addition
-    | Const a::Addition::Const b::xs -> Const (a+b)::xs  |> reduce precedence_for_addition
-    | Const a::Multiplication::SubExpression l::xs ->
-        let tmp = reduce precedence_for_addition l
-        let newExpr = if tmp.Length = 1 then tmp else [SubExpression tmp]
-        Const a::Multiplication::tmp@xs |> reduce precedence_for_addition
-    | Const a::Addition::SubExpression l::xs ->
-        let tmp = reduce  precedence_for_addition l
-        let newExpr = if tmp.Length = 1 then tmp else [SubExpression tmp]
-        Const a::Addition::tmp@xs |> reduce precedence_for_addition
-    | SubExpression l::xs ->
-        let tmp = reduce precedence_for_addition l
-        let newExpr = if tmp.Length = 1 then tmp else [SubExpression tmp]
-        newExpr@xs |> reduce precedence_for_addition
-    | _ -> failwith "Unexpected"
+    let reduction =
+        match expr with
+        | Const a::[]                           -> [Const a]
+        | Const a::Multiplication::Const b::xs  -> Const (a*b)::xs
+        | Const a::Addition::Const b::xs        -> Const (a+b)::xs
+        | Const a::b::Expression inner::xs      -> Const a::b::reduce precedence inner@xs
+        | Expression inner::xs                  -> reduce precedence inner@xs
+        | _ -> failwith "Unexpected"
+    if reduction.Length = 1 then
+        reduction
+    else
+        reduce precedence  reduction
 
-let doHomework addition_precedence =
+let doHomework precedence =
     input
-    |> Array.map parse
-    |> Array.map (reduce addition_precedence)
-    |> Array.collect(fun c -> [|Addition; c.Head|])
-    |> Array.tail
     |> Array.toList
-    |> reduce addition_precedence
-    |> List.exactlyOne
-    |> extractValueFromConst
+    |> List.map parse
+    |> List.collect (reduce precedence)
+    |> List.sumBy extractValueFromConst
 
-let part1 = doHomework false
-let part2 = doHomework true
+let part1 = doHomework Left_to_right
+let part2 = doHomework Addition_has_precedence
