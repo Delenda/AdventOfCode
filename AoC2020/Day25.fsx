60a80f9d840331a72ed2829aa7335279411a8733
Delenda <you@example.com>
2020-12-25 19:45:56 +0100

    Day25

---
+++ AoC2020/Day25.fsx
@@ -0,0 +1,21 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ +   @"\Input\Day25.txt") 
+let card_pk = input.[0] |> uint64
+let door_pk = input.[1] |> uint64
+let modulus = 20201227UL
+
+let loop_size pk = 
+    Seq.initInfinite id
+    |> Seq.scan(fun s _ -> (7UL*s)%modulus) 1UL
+    |> Seq.takeWhile(fun s -> s <> pk)
+    |> Seq.length
+    |> uint64
+
+let card_ls = loop_size card_pk
+
+let encrypt subject (loopsize:uint64) = 
+    Seq.initInfinite id
+    |> Seq.scan(fun s _ -> (subject*s)%modulus) 1UL
+    |> Seq.take (int loopsize + 1)
+    |> Seq.last
+
+let part1 = encrypt door_pk card_ls
