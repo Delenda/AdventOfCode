48aa45c9cb266b0955a4a68280c06d67c5bd31e5
Delenda <you@example.com>
2021-01-05 01:57:26 +0100

    Day16

---
+++ AoC2016/Day16.fsx
@@ -0,0 +1,29 @@
+﻿let input = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__+ @"\Input\Day16.txt")
+
+let iteration (a:int list) = 
+    a@[0]@(a |> List.rev |> List.map(fun x -> 1 - x))
+
+let initial = 
+    input.ToCharArray() 
+    |> Seq.map(fun c -> int c - int '0')
+    |> Seq.toList
+
+let rec checksum (data : int list)= 
+    if data.Length%2 = 1 then data else
+    data 
+    |> List.chunkBySize 2
+    |> List.map(fun l -> if l.Head = l.Tail.Head then 1 else 0)
+    |> checksum
+    
+let fillDisk size = 
+    Seq.initInfinite id
+    |> Seq.scan (fun s _ -> iteration s) initial
+    |> Seq.skipWhile(fun l -> l.Length < size)
+    |> Seq.head
+    |> List.take size
+    |> checksum
+    |> List.map string
+    |> String.concat ""
+
+let part1 = fillDisk 272
+let part2 = fillDisk 35651584 // ~30 seconds
