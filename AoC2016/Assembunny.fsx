92f4db01939a519df0daf606eea27c522d8c6988
Delenda <you@example.com>
2021-01-10 00:29:50 +0100

    Day23

---
+++ AoC2016/Assembunny.fsx
@@ -0,0 +1,93 @@
+﻿type register = A|B|C|D
+
+type value = 
+    | Register of register
+    | Litteral of int64
+
+type opcode = 
+    | Cpy of value*value
+    | Inc of value
+    | Dec of value
+    | Jnz of value*value
+    | Tgl of value
+
+let parse_register (term:string) =
+    match term with
+    | "a" -> A
+    | "b" -> B
+    | "c" -> C
+    | "d" -> D
+    | _ -> failwith ("unexpected register")
+
+let parse_value (term:string) =
+    match term with
+    | "a" -> Register A
+    | "b" -> Register B
+    | "c" -> Register C
+    | "d" -> Register D
+    | _ -> term |> int64 |> Litteral 
+
+let parse (line:string) =
+    let terms = line.Split(' ')
+    match terms.[0] with
+    | "cpy" -> Cpy ((parse_value terms.[1]),(parse_register terms.[2] |> Register))
+    | "inc" -> Inc (parse_register terms.[1] |> Register)
+    | "dec" -> Dec (parse_register terms.[1] |> Register)
+    | "jnz" -> Jnz (parse_value terms.[1], parse_value terms.[2])
+    | "tgl" -> Tgl (parse_value terms.[1])
+    | _ -> failwith ("Unexpected code: " + line)
+
+type state = {Pointer: int64 ; Register :Map<register,int64>; Program : Map<int64, opcode>}
+
+let eval (register:Map<register,int64>) v = 
+    match v with
+    | Litteral l -> l
+    | Register r -> register.[r]
+
+let toggle = function
+    | Cpy(v,r) -> Jnz(v,r)
+    | Inc r -> Dec r
+    | Dec r -> Inc r
+    | Jnz(v,offset) -> Cpy(v, offset)
+    | Tgl v -> Inc v
+    
+let isRegister = function
+    | Litteral _ -> false
+    | Register _ -> true
+
+let canExecute = function
+    | Cpy(v,r) -> isRegister r
+    | Inc r -> isRegister r
+    | Dec r -> isRegister r
+    | Jnz(v,offset) -> true
+    | Tgl v -> true
+
+let register = function
+    | Register x -> x
+    | Litteral _ -> failwith("Value is not a register") 
+    
+let run (program:opcode array) (reg, regvalue) = 
+    Seq.unfold(fun state ->
+        if state.Pointer >= int64 state.Program.Count then None else
+        let newRegister, offset, newProgram = 
+            let opcode = state.Program.[state.Pointer]
+            if canExecute opcode then
+                match state.Program.[state.Pointer] with
+                | Cpy (v, r) -> state.Register.Add(register r, eval state.Register v), 1L, state.Program
+                | Inc r -> state.Register.Add(register r, state.Register.[register r] + 1L), 1L, state.Program
+                | Dec r -> state.Register.Add(register r, state.Register.[register r] - 1L), 1L, state.Program
+                | Jnz (v,offset) -> state.Register, (if eval state.Register v <> 0L then (eval state.Register offset) else 1L), state.Program
+                | Tgl v -> 
+                    let idx = state.Pointer + eval state.Register v
+                    if idx < 16L then failwith ("Unexpected toggle")
+                    if idx < 0L || idx >= int64 state.Program.Count then
+                        state.Register, 1L, state.Program
+                    else
+                        state.Register, 1L, state.Program.Add(state.Pointer + eval state.Register v, toggle state.Program.[state.Pointer + eval state.Register v])
+            else
+                state.Register, 1L, state.Program
+                
+        let newState = {Pointer = state.Pointer + offset; Register = newRegister; Program = newProgram}
+        Some(newState.Register.[A], newState)
+        ) {Pointer = 0L; Register = ([A,0L;B,0L;C,0L;D,0L] |> Map).Add(reg,regvalue); Program = program |> Seq.mapi(fun i c-> int64 i, c) |> Map}
+    |> Seq.last
---
+++ AoC2016/Day23.fsx
@@ -0,0 +1,29 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\Input\Day23.txt") 
+
+#load "Assembunny.fsx"
+
+open Assembunny
+
+let program = 
+    input 
+    |> Array.map parse
+
+let part1 = run program (A,7L) 
+
+let part2 = 
+    let rec fac n = 
+        if n = 1L then 1L else n * fac (n-1L)
+    
+    let modifiedProgram = 
+        program
+        |> Array.mapi(fun i opcode -> 
+            if i = 0 then
+                Cpy  (Litteral (fac 12L),Register A)
+            else if i = 1 then
+                Jnz (Litteral 1L, Litteral 15L)
+            else if i = 24 || i = 22 || i = 20 || i =18 
+                then toggle opcode 
+            else
+                opcode
+            )
+    run modifiedProgram (A,0L)
