e4f4a9381c6457be5f6d4e8451b0835bd330ed92
Delenda <you@example.com>
2021-12-01 13:38:32 +0100

    Aoc2021 Day01

---
+++ AoC2021/Day01.fsx
@@ -0,0 +1,16 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\Input\Day01.txt") |> Seq.map int
+
+let countIncrease = 
+    Seq.windowed 2
+    >> Seq.filter(fun pair -> pair.[1] > pair.[0])
+    >> Seq.length
+
+let part1 = 
+    input
+    |> countIncrease
+
+let part2 = 
+    input
+    |> Seq.windowed 3
+    |> Seq.map Array.sum
+    |> countIncrease
