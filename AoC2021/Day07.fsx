78b79a1969f87662697753cbf3162a4c49c8845c
Delenda <you@example.com>
2021-12-07 10:40:04 +0100

    Day07

---
+++ AoC2021/Day07.fsx
@@ -0,0 +1,13 @@
+﻿let input = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + @"\Input\Day07.txt")
+
+let totalFuel fuelMetric = 
+    let crabs = input.Split(',') |> Array.toList |> List.map int
+    let max = crabs |> List.max
+    let min = crabs |> List.min
+    [min..max] |> List.map(fun destination -> crabs |> List.sumBy(fun crabPostion -> fuelMetric destination crabPostion)) |> List.min
+
+let dist d c = abs(d-c)
+let triangleDist d c = abs(d-c) * (abs(d-c)+1) / 2
+
+let part1 = totalFuel dist
+let part2 = totalFuel triangleDist
